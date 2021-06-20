a900cc1693f0f12f54957efc9d2d53a83d79f330
Amrinder singh <amrinders433@gmail.com>
2021-06-20 13:52:38 -0400

    form

--- forms/contact.php
+++ forms/contact.php
@@ -21,6 +21,7 @@
   $contact->to = $receiving_email_address;
   $contact->from_name = $_POST['name'];
   $contact->from_email = $_POST['email'];
+  $contact->subject = $_POST['subject'];
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
   /*
@@ -34,8 +35,6 @@
 
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['reference'], 'reference');
-  $contact->add_message( $_POST['phoneNumber'], 'phoneNumber');
   $contact->add_message( $_POST['message'], 'Message', 10);
 
   echo $contact->send();
