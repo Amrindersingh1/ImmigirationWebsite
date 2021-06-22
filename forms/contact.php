64448d1b1ece3a4ff85885ca5e8f9ae1b5bd705a
Amrinder singh <amrinders433@gmail.com>
2021-06-22 16:25:49 -0400

    s

--- forms/contact.php
+++ forms/contact.php
@@ -7,8 +7,7 @@
   */
 
   // Replace contact@example.com with your real receiving email address
-  if(isset($_POST['submit'])) {
-  $receiving_email_address = 'info@oceanfrontimmigration.ca';
+  $receiving_email_address = 'amrinders433@gmail.com';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
@@ -22,22 +21,23 @@
   $contact->to = $receiving_email_address;
   $contact->from_name = $_POST['name'];
   $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
+  $contact->subject = 'Contact From https://oceanfrontimmigration.com/';
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
   $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
+    'host' => 'mail.oceanfrontimmigration.com',
+    'username' => '_mainaccount@oceanfrontimmigration.com',
+    'password' => 'Cambridge1',
+    'port' => '465'
   );
-  */
+  
 
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
+  $contact->add_message( $_POST['name'], 'phoneNumber');
+  $contact->add_message( $_POST['email'], 'reference');
+  $contact->add_message( $_POST['subject'], 'subject');
+  $contact->add_message( $_POST['message'], 'Message');
 
   echo $contact->send();
-}
 ?>
