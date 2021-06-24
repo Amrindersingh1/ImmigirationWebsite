8b2aac9845b14d32bca7794f58389027352295a0
Amrinder singh <amrinders433@gmail.com>
2021-06-23 21:38:25 -0400

    changes

--- forms/contact.php
+++ forms/contact.php
@@ -1,43 +1,9 @@
 <?php
-  /**
-  * Requires the "PHP Email Form" library
-  * The "PHP Email Form" library is available only in the pro version of the template
-  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
-  * For more info and help: https://bootstrapmade.com/php-email-form/
-  */
+  $to = 'info@oceanfrontimmigration.ca';
+  $subject = "Details From website:https://oceanfrontimmigration.com/";
+  $txt = "Contact Details From website: "."\r\n"."Email : ".$_POST['email']."\r\n"."Name : ".$_POST['name']."\r\n"."phoneNumber : ".$_POST['phoneNumber']."\r\n"."Reference : ".$_POST['reference']."\r\n"."Subject : ".implode(" , ", $_POST['subject'])."\r\n"."Message : ".$_POST['message'];
+  $headers = "From: ".$_POST['email'];
 
-  // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'amrinders433@gmail.com';
-
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
-  }
-
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
-  
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = 'Contact From https://oceanfrontimmigration.com/';
-
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  $contact->smtp = array(
-    'host' => 'mail.oceanfrontimmigration.com',
-    'username' => '_mainaccount@oceanfrontimmigration.com',
-    'password' => 'Cambridge1',
-    'port' => '465'
-  );
-  
-
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['name'], 'phoneNumber');
-  $contact->add_message( $_POST['email'], 'reference');
-  $contact->add_message( $_POST['subject'], 'subject');
-  $contact->add_message( $_POST['message'], 'Message');
-
-  echo $contact->send();
+  mail($to,$subject,$txt,$headers);
+  return true
 ?>
